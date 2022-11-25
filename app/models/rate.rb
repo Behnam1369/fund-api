d2a8ebe8b980e21f3a47d6e1581a04c8c9f8561b
behnam1369 <behnam.aghaali@yahoo.com>
2022-11-25 21:40:55 +0330

    Rates

---
+++ app/models/rate.rb
@@ -0,0 +1,24 @@
+class Rate < ApplicationRecord
+  belongs_to :asset
+
+  after_create :update_pending_profit
+
+  def update_pending_profit
+    cash = Investment.sum(:amount) - Transaction.sum(:amount)
+
+    ass = Asset.find_by(id: self.asset.id)
+    ass.price = self.price
+    ass.amount = ass.qty * ass.price
+    ass.save
+
+    assets_amount = Asset.sum(:amount) * 0.99
+    total = cash + assets_amount
+    total_calculated_profit = User.sum(:investment) + User.sum(:calculated_profit)
+    total_pending_profit = total - total_calculated_profit
+
+    User.all.each do |user|
+      user.pending_profit = user.share * total_pending_profit
+      user.save
+    end
+  end
+end
